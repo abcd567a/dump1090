97fcbc81e32c8556b84f2fb808ea7453f587aab7
Oliver Jowett <oliver.jowett@flightaware.com>
2021-01-21 12:00:53 +0800

    Switch to the new UC8 zero offset in benchmarks, fix some bugs

--- dsp/benchmark/magnitude_uc8_benchmark.c
+++ dsp/benchmark/magnitude_uc8_benchmark.c
@@ -16,8 +16,8 @@ void STARCH_BENCHMARK(magnitude_uc8) (void)
     // 0.9 magnitude, varying phase
     double degrees = 0;
     for (; i < len && degrees < 360; i += 1, degrees += 1) {
-        in[i].I = (uint8_t) (0.9 * cos(degrees * M_PI / 180.0) * 127.5 + 127.5);
-        in[i].Q = (uint8_t) (0.9 * sin(degrees * M_PI / 180.0) * 127.5 + 127.5);
+        in[i].I = (uint8_t) (0.9 * cos(degrees * M_PI / 180.0) * 128 + 127.4);
+        in[i].Q = (uint8_t) (0.9 * sin(degrees * M_PI / 180.0) * 128 + 127.4);
     }
 
     // 0, 45, 90 degree phase, full input range
@@ -49,27 +49,28 @@ void STARCH_BENCHMARK(magnitude_uc8) (void)
 
 bool STARCH_BENCHMARK_VERIFY(magnitude_uc8) (const uc8_t *in, uint16_t *out, unsigned len)
 {
-    const double max_error = 0.025; // tolerate 2.5% error
-    const double epsilon = 2.0;
+    const double max_error = 0.015; // tolerate 1.5% error
+    const double epsilon = 3.0;
     bool okay = true;
 
     for (unsigned i = 0; i < len; ++i) {
-        double I = (in[i].I - 127.5) / 127.5;
-        double Q = (in[i].Q - 127.5) / 127.5;
+        double I = (in[i].I - 127.4) / 128;
+        double Q = (in[i].Q - 127.4) / 128;
         double magsq = I * I + Q * Q;
-        if (magsq > 1)
-            magsq = 1;
-        double expected = sqrt(magsq);
-        double actual = out[i] / 65535.0;
+        double expected = round(sqrt(magsq) * 65536.0);
+        if (expected > 65535.0)
+            expected = 65535.0;
+        double actual = out[i];
 
         double error = fabs(expected - actual);
         double error_fraction = error / (expected > epsilon ? expected : epsilon);
-        if (fabs(error) > epsilon && fabs(error_fraction) > max_error) {
-            fprintf(stderr, "verification failed: in[%u].I=%u in[%u].Q=%u out[%u]=%u expected=%.0f, error=%.2f%%\n",
+        if (error > epsilon && error_fraction > max_error) {
+            fprintf(stderr, "verification failed: in[%u].I=%u in[%u].Q=%u out[%u]=%u, expected=%.0f, error=%.2f%%\n",
                     i, in[i].I,
                     i, in[i].Q,
                     i, out[i],
-                    expected, error_fraction * 100.0);
+                    expected,
+                    error_fraction * 100.0);
             okay = false;
         }
     }
