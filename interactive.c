9c2531a98d6a385bc3a04cc7a97427740f8a6b10
VasiliyTurchenko <34768220+VasiliyTurchenko@users.noreply.github.com>
2021-03-08 11:34:56 +0300

    fixed screen (first line) issue when in the interactive mode (#116)

--- interactive.c
+++ interactive.c
@@ -137,6 +137,7 @@ void interactiveNoConnection(void) {
 void interactiveShowData(void) {
     struct aircraft *a = Modes.aircrafts;
     static uint64_t next_update;
+    static bool need_clear = true;
     uint64_t now = mstime();
     char progress;
     char spinner[4] = "|/-\\";
@@ -149,6 +150,10 @@ void interactiveShowData(void) {
     if (!Modes.interactive)
         return;
 
+    if (need_clear) {
+        clear();
+        need_clear = false;
+    }
     // Refresh screen every (MODES_INTERACTIVE_REFRESH_TIME) miliseconde
     if (now < next_update)
         return;
